ca4f7194ce698d37a77fed2299be8e4ca93eac75
Imuzuin <css81@naver.com>
2019-07-02 15:17:54 +0900

    androidx 기반 BottomSheet 샘플 코드 구현

--- app/src/main/java/com/polaris/demo/bottomsheet/MenuActivity.java
+++ app/src/main/java/com/polaris/demo/bottomsheet/MenuActivity.java
@@ -1,14 +1,61 @@
 package com.polaris.demo.bottomsheet;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private android.widget.Button btnPersistent;
+    private android.widget.Button btnModal;
+    private Toolbar toolbar;
+    private Button btnModalFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        setContentView(R.layout.activity_menu);
+        this.btnModalFragment = (Button) findViewById(R.id.btnModalFragment);
+        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Bottom Sheet");
+        this.btnModal = (Button) findViewById(R.id.btnModal);
+        this.btnPersistent = (Button) findViewById(R.id.btnPersistent);
+
+        btnPersistent.setOnClickListener(this);
+        btnModal.setOnClickListener(this);
+        btnModalFragment.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()) {
+            case R.id.btnPersistent:
+                intent = new Intent(this, PersistentActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.btnModal:
+                View dialogView = getLayoutInflater().inflate(R.layout.bottom_sheet, null);
+                BottomSheetDialog dialog = new BottomSheetDialog(this);
+                dialog.setContentView(dialogView);
+                dialog.show();
+                break;
+
+            case R.id.btnModalFragment:
+                BottomSheetFragment bottomSheetFragment = new BottomSheetFragment();
+                bottomSheetFragment.show(getSupportFragmentManager(), bottomSheetFragment.getTag());
+                break;
+        }
     }
 }
